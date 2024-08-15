34bccdd2589a1804e16148f328afa7e39667c8af
Duy Toan <toanpmo123@gmail.com>
2024-08-15 19:09:53 +0700

    update debug nestjs

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+
+import { IsNotEmpty } from "class-validator";
+
+export class CreateUserDto {
+    @IsNotEmpty({message: 'name không được để trống'})
+    name: string;
+    email: string;
+    password: string;
+    phone: string;
+    address: string;
+    image: string;
+}
