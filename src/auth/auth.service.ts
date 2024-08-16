f8c1a2c4d02ad4491e2283a299cee7e09145355c
Duy Toan <toanpmo123@gmail.com>
2024-08-16 12:12:28 +0700

    add accesstoken login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,29 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '@/modules/users/users.service';
+import { comparePasswordHelper } from '@/helpers/utils';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) { }
+
+  findAll // instead of the user object
+    () {
+    throw new Error('Method not implemented.');
+  }
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) { }
 
   async signIn(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(username);
-    if (user?.password !== pass) {
+    const isValidPassword = await comparePasswordHelper(pass, user.password);
+    if (!isValidPassword) {
       throw new UnauthorizedException();
     }
-    const { password, ...result } = user;
-    // TODO: Generate a JWT and return it here
-    // instead of the user object
-    return result;
+    const payload = { sub: user._id, username: user.email };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
